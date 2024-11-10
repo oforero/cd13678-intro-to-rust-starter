a982bedd28476a83337bb7774e43b1c5b81aa5fe
Oscar Mauricio Forero Carrillo <o.forero@icloud.com>
2024-11-10 20:23:01 +0100

    build: Automatically generate binding to the opengl wrapper library

--- game_engine/build.rs
+++ game_engine/build.rs
@@ -1,6 +1,32 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("opengl_libc/opengl_wrapper_lib/opengl_wrapper_lib.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .clang_arg("-Iopengl_libc/include")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
     cc::Build::new()
         .file("./opengl_libc/opengl_wrapper_lib/opengl_wrapper_lib.c")
         .include("./opengl_libc/opengl_wrapper_lib/")
         .compile("opengl_wrapper_lib.o");
+
 }
