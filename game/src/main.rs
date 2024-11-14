4e7cc5636e29eb1d7024e216d92896d9d144fa73
Oscar Mauricio Forero Carrillo <o.forero@icloud.com>
2024-11-15 00:01:23 +0100

    Task 6: Remove the initial simple game used to test the bindings

--- game/src/main.rs
+++
@@ -1,9 +0,0 @@
-use game_engine::*;
-
-fn main() {
-    create_game_window("C Test Game", 800, 600);
-    while !window_should_close() {
-        update_game_window();
-        std::thread::sleep(std::time::Duration::from_millis(16)); // Optional: Sleep to reduce CPU usage
-    }
-}
